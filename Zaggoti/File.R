6cc6ca53a1823677780c7c2d479ddee548351680
Karim Zaggoti <karimzaggoti@gmail.com>
2025-06-05 14:03:29 -0400

    commit

--- Zaggoti/File.R
+++ Zaggoti/File.R
@@ -1,6 +1,1 @@
-install.packages("usethis")
-usethis::use_git_config(user.name = "Karim Zaggoti", 
-                        user.email = "karimzaggoti@gmail.com")
-usethis::create_github_token()
-install.packages("gitcreds")
-gitcreds::gitcreds_set()
+#Zaggoti
