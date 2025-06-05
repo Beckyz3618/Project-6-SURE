42e061c5aaed9d1a0a1c647e95364966d4c67aca
BeckyZhou <Beckyz3618@gmail.com>
2025-06-05 14:12:44 -0400

    testing2

--- Zhou/Doc1.R
+++ Zhou/Doc1.R
@@ -1,1 +1,8 @@
-#hello world
+# uncomment and run the following line to install the usethis package
+# install.packages("usethis")
+usethis::use_git_config(user.name = "becky zhou", 
+                        user.email = "Insert Your Email")
+# uncomment and run the following line to install the gitcreds package
+# install.packages("gitcreds")
+gitcreds::gitcreds_set()
+
