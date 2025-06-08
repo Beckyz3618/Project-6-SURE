e16e5b2b3da8910ea97f71f558f8d8bf82a00039
Karim Zaggoti <karimzaggoti@gmail.com>
2025-06-08 15:18:37 -0400

    commit

--- Zaggoti/File.R
+++ Zaggoti/File.R
@@ -1,17 +1,5 @@
 library(tidyverse)
 covid_hospitalizations <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/covid_hospitalizations.csv")
-head(covid_hospitalizations)
-view(covid_hospitalizations)
 
-covid_hospitalizations |>
-  ggplot(aes(x=date, y= covid_patients_mean))+
-  geom_line()
 
-dim(covid_hospitalizations)
-class(covid_hospitalizations)
-head(covid_hospitalizations)
-
-table(covid_hospitalizations$county)
-summary(covid_hospitalizations$date)
-
-filter(covid_hospitalizations, date == 2022-01-01 & county == Philadelphia)
+skim(covid_hospitalizations)
