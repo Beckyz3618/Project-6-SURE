192bfb5b5958eafa8330c98a5a19ea828932f12b
Karim Zaggoti <karimzaggoti@gmail.com>
2025-06-09 14:39:17 -0400

    Allegheny plot

--- Zaggoti/File.R
+++ Zaggoti/File.R
@@ -1,5 +1,43 @@
 library(tidyverse)
+library(skimr)
 covid_hospitalizations <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/covid_hospitalizations.csv")
 
-
 skim(covid_hospitalizations)
+
+
+covid_hospitalizations <- covid_hospitalizations |> 
+  mutate(
+    date      = as.Date(sub("T.*", "", date)),         
+    vent_ratio = covid_vents / covid_patients,
+    icu_load   = covid_icu   / icu_total,               
+    icu_slack  = icu_avail   / icu_total,               
+    med_slack  = med_avail   / med_total
+  )
+  
+Allegheny <- covid_hospitalizations |>
+  filter(county == "Allegheny")
+
+ggplot(Allegheny, aes(date, icu_load)) +
+  geom_line(color = "steelblue") +
+  labs(title = "ICU load – Allegheny County",
+       y = "% ICU beds filled by COVID")
+
+
+monthly_peak <- covid_hospitalizations |>
+  mutate(month = as.Date(format(date, "%Y-%m-01"))) |>
+  group_by(county, month) |>
+  summarise(peak_icu = max(icu_load),
+            latitude = first(latitude, na.rm = TRUE),                 
+            groups  = "drop")
+
+
+
+
+  
+
+
+
+
+
+
+
