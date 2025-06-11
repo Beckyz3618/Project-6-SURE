3ae1da60959ad5b5c3f8d9723aeb39e8e6779f20
Victoria Nguyen <victoriang893@gmail.com>
2025-06-11 14:16:07 -0400

    covid icu df cleaned of NA values

---
+++ Nguyen/covid-icu-clean.R
@@ -0,0 +1,19 @@
+# cleaning NA values in df_covid_icu
+view(df_covid_icu)
+
+vis_miss(df_covid_icu, warn_large_data = FALSE)
+dim(df_covid_icu)
+
+# last 10 rows that have NA value 
+df_covid_icu[tail(which(is.na(df_covid_icu$covid_icu)), 10),]
+
+tail(which(is.na(df_covid_icu$covid_icu)),10) #11820 is last one
+# which(is.na(df$my_column)) # find rows with NA
+# last_10_na_rows <- tail(na_rows, 10) # get index or row num of last 10 NA values of column
+# df[last_10_na_rows, ] # view those rows
+
+view(df_covid_icu[11815:11830,]) # after 2020-10-23 has covid_icu count
+
+# grab nrow after 2020-10-23 or row num 11821
+df_covid_icu = df_covid_icu[11821:nrow(df_covid_icu),]
+view(df_covid_icu)
